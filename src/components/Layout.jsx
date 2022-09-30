04a2f1c65d3a0242732e32639ee6d9ab0617c9bb
Pavel <KozhevnikovPA295@gmail.com>
2022-09-30 12:51:56 +0300

    do not touch

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -18,6 +18,7 @@ export default function Layout({ initState }) {
         <script defer src="/js/app.js" />
         <script defer src="/js/vendor.js" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossOrigin="anonymous" />
         <link href="/styles/main.css" rel="stylesheet" />
         <link rel="stylesheet" href="/App.css" />
 
--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -1,3 +1,36 @@
+// import React from 'react';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { NavLink } from 'react-router-dom';
+
+// function Navigationbar({ currUser, logOutHandler }) {
+//   return (
+//     <Navbar bg="light" expand="lg">
+//       <Container>
+//         <Navbar.Brand href="/"><img src="/images/logo.png" alt="logo" style={{ width: 150, height: 150 }} /></Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link  to="/" style={{ fontFamily: "Arial", fontWeight: "bold", fontSize: 20}}>Home</NavLink>
+
+//             <.span >
+//               {!currUser.id ? <Nav.Link  to="/user/registration">registration</NavLink> : currUser.name }
+//             </span>
+
+//             {currUser.id ? (
+//               <Nav.Link  onClick={logOutHandler}>Logout</NavLink>
+//             ) : (
+//               <Nav.Link  to="/user/authorization">authorization</NavLink>
+//             )}
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+// export default Navigationbar;
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,22 +39,19 @@ import { NavLink } from 'react-router-dom';
 
 function Navigationbar({ currUser, logOutHandler }) {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" variant="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/"><img src="/images/logo.png" alt="logo" style={{ width: 150, height: 150 }} /></Navbar.Brand>
+        <Navbar.Brand href="/"><img src="/images/logo.png" alt="logo" style={{ width: 100, height: 100 }} /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink className="nav-link" to="/" style={{ fontFamily: "Arial", fontWeight: "bold", fontSize: 20}}>Home</NavLink>
-
-            <span className="nav-link">
-              {!currUser.id ? <NavLink className="nav-link" to="/user/registration">registration</NavLink> : currUser.name }
-            </span>
+            <Nav.Link to="/" style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize: 20 }}>Home</Nav.Link>
+            {!currUser.id ? <Nav.Link to="/user/registration" style={{fontFamily: 'Arial', fontWeight: 'bold', fontSize: 20 }}>Registration</Nav.Link> : currUser.name }
 
             {currUser.id ? (
-              <NavLink className="nav-link" onClick={logOutHandler}>Logout</NavLink>
+              <Nav.Link onClick={logOutHandler} style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize: 20 }}>Logout</Nav.Link>
             ) : (
-              <NavLink className="nav-link" to="/user/authorization">authorization</NavLink>
+              <Nav.Link to="/user/authorization" style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize: 20 }}>Authorization</Nav.Link>
             )}
           </Nav>
         </Navbar.Collapse>
