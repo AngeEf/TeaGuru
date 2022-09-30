e88816f9f8aca0bc181f155f1bcd65e5b68ce755
Vadim Sidorov <vadim.06041974@gmail.com>
2022-09-30 14:26:13 +0300

    fix nav

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/registration', (req, res) => {
   res.render('Layout', { });
 });
+
 router.get('/cardpage', (req, res) => {
   res.render('Layout', { });
 });
